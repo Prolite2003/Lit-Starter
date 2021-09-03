c3887b4ecd9233d1911781766756e90665b244a6
Stefan SChooneveld <325849@student.mboutrecht.nl>
2021-09-03 13:15:33 +0200

    feat(mo-portfolio): made a new js file and a new element
    i made a new js file for my portfolio and in there i made a new element for my portfolio website
    that im planning on building soon.

---
+++ components/mo-portfolio.js
@@ -0,0 +1,21 @@
+import {LitElement, html} from 'lit-element';
+
+
+
+
+class MyOwnPortfolio extends LitElement {
+    static get style() {
+        return css`
+        
+        
+    `};
+
+    render() {
+        return html`
+        
+
+
+        `};
+    
+}
+customElements.define('mo-port', MyOwnPortfolio);
